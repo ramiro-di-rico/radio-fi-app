eb80483404c0a8838747d80f0215aeb3b27f4a46
Ramiro Di Rico <ramiro.di.rico@gmail.com>
2021-07-09 18:09:45 -0300

    decouple app info into its own widget

---
+++ lib/controls/app-info.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
+
+class AppInfoWidget extends StatefulWidget {
+  @override
+  _AppInfoWidgetState createState() => _AppInfoWidgetState();
+}
+
+class _AppInfoWidgetState extends State<AppInfoWidget> {
+  String appName = "";
+  String packageName = "";
+  String version = "";
+  String buildNumber = "";
+  bool infoLoaded = false;
+
+  Future _getBuildInfo() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+    setState(() {
+      appName = packageInfo.appName;
+      packageName = packageInfo.packageName;
+      version = packageInfo.version;
+      buildNumber = packageInfo.buildNumber;
+      infoLoaded = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _getBuildInfo();
+    return Card(
+      child: infoLoaded
+          ? Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 5),
+                        child: Text(
+                          "Application Information",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 5),
+                        child: Text("App name : $appName"),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 5),
+                        child: Text("Package: $packageName"),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 5),
+                        child: Text("Version: $version"),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 5),
+                        child: Text("Build number $buildNumber"),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          : CircularProgressIndicator(),
+    );
+  }
+}
--- lib/info-scree.dart
+++ lib/info-scree.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:package_info/package_info.dart';
-
+import 'controls/app-info.dart';
 import 'controls/country-code-selector.dart';
 
 class InfoScreen extends StatefulWidget {
@@ -11,32 +10,8 @@ class InfoScreen extends StatefulWidget {
 }
 
 class _InfoScreenState extends State<InfoScreen> {
-  String appName = "";
-  String packageName = "";
-  String version = "";
-  String buildNumber = "";
-  bool infoLoaded = false;
-
-  Future _getBuildInfo() async {
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-
-    setState(() {
-      appName = packageInfo.appName;
-      packageName = packageInfo.packageName;
-      version = packageInfo.version;
-      buildNumber = packageInfo.buildNumber;
-      infoLoaded = true;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    _getBuildInfo();
     return Scaffold(
       appBar: AppBar(
         title: Text("Radio App"),
@@ -47,63 +22,7 @@ class _InfoScreenState extends State<InfoScreen> {
           child: Column(
             children: [
               CountryCodeSelector(),
-              Card(
-                child: infoLoaded
-                    ? Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 5),
-                                  child: Text(
-                                    "Application Information",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 5),
-                                  child: Text("App name : $appName"),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 5),
-                                  child: Text("Package: $packageName"),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 5),
-                                  child: Text("Version: $version"),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 5),
-                                  child: Text("Build number $buildNumber"),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      )
-                    : CircularProgressIndicator(),
-              ),
+              AppInfoWidget(),
             ],
           ),
         ),
