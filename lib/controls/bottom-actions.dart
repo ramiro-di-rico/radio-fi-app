1b0ade32754a232bb71eaf180ecbaa701ef212fc
Ramiro Di Rico <rdirico@makingsense.com>
2021-07-09 22:50:24 -0300

    move controls to bottom bar widget and remove stations control

--- lib/controls/bottom-actions.dart
+++ lib/controls/bottom-actions.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import '../data/station-controller.dart';
-import 'station-controls.dart';
 
 class BottomActionWidget extends StatefulWidget {
   @override
@@ -10,11 +9,13 @@ class BottomActionWidget extends StatefulWidget {
 
 class _BottomActionWidgetState extends State<BottomActionWidget> {
   StationsController _stationsController = GetIt.instance<StationsController>();
+  double volume = 1.0;
 
   @override
   void initState() {
     super.initState();
     _stationsController.addListener(updateStationsList);
+    updateVolume();
   }
 
   @override
@@ -25,12 +26,60 @@ class _BottomActionWidgetState extends State<BottomActionWidget> {
 
   @override
   Widget build(BuildContext context) {
+    var currentStation = _stationsController.getCurrentStation();
+
+    if (currentStation == null) return Container();
+
     return Container(
-        color: Theme.of(context).bottomAppBarColor,
-        child: StationsControlsWidget());
+      color: Theme.of(context).bottomAppBarColor,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: TextButton(
+              onPressed: () => _stationsController.displayCurrentStation(),
+              child: Text(currentStation.name.length > 10
+                  ? currentStation.name.substring(0, 10)
+                  : currentStation.name),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Slider(
+              value: volume,
+              onChanged: setVolume,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: TextButton(
+              child: Icon(Icons.stop),
+              onPressed: () async {
+                setState(() {
+                  _stationsController.stop();
+                });
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   void updateStationsList() {
-    setState(() {});
+    setState(() {
+      updateVolume();
+    });
+  }
+
+  void updateVolume() {
+    volume = _stationsController.getVolume();
+  }
+
+  void setVolume(double vol) {
+    setState(() {
+      volume = vol;
+      _stationsController.changeVolume(vol);
+    });
   }
 }
