f33e827319aecede9aa3b7ebf04e0a25ef33c541
Ramiro Di Rico <ramiro.di.rico@gmail.com>
2022-08-03 20:56:24 -0300

    update picture widget when no image url provided

--- lib/controls/picture-widget.dart
+++ lib/controls/picture-widget.dart
@@ -7,22 +7,24 @@ class PictureWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.network(
-              imageUrl,
-              width: 30,
-              errorBuilder: (context, o, trace) {
-                return Icon(Icons.radio_sharp);
-              },
-            ),
-          ),
-        ),
-      ],
-    );
+    return imageUrl.isNotEmpty && Uri.parse(imageUrl).isAbsolute
+        ? Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    imageUrl,
+                    width: 30,
+                    errorBuilder: (context, o, trace) {
+                      return Icon(Icons.radio_sharp);
+                    },
+                  ),
+                ),
+              ),
+            ],
+          )
+        : Icon(Icons.radio_sharp);
   }
 }
