e31764ba2dbca66082098f52f047bd60ae7a82f5
Ramiro Di Rico <ramiro.di.rico@pm.me>
2024-09-14 18:04:14 -0300

    Flutter upgrade

--- lib/services/station-fetcher.dart
+++ lib/services/station-fetcher.dart
@@ -4,7 +4,7 @@ abstract class StationFetcher {
   Future<List<Station>> getStations();
 }
 
-abstract class GeoStationFetcher {
+abstract mixin class GeoStationFetcher {
   Future<List<Station>> getStationsByCountryCode(String countryCode);
   void SetActiveFiltering(bool active);
 }
